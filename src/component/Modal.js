33a0bab2676470613a4231f5ad6093a6d5bbbaf0
ragicode3056 <raginijayakumar96@gmail.com>
2022-02-10 19:57:19 +0530

    css updated for popup warning modal

--- src/component/Modal.js
+++ src/component/Modal.js
@@ -2,8 +2,10 @@ export const Modal = () => {
     return (
         <div className='boxContanier-modal'>
             <p>Are you sure want to deleted</p>
-            <button className='btn-cancel'> Cancel</button>
-            <button className='btn'>Confirm</button>
+            <div className="btn-div">
+                <button className='btn-cancel'> Cancel</button>
+                <button className='btn'>Confirm</button>
+            </div>
         </div>
     )
 }
